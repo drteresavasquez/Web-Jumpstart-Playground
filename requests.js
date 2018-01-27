9b196d622dedb772ea8c4c1f7ec94f74aed90339
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-01-27 12:02:45 -0600

    supehero site request page

---
+++ requests.js
@@ -0,0 +1,40 @@
+console.log("requests");
+
+let vols = ["Teresa", "Trinity", "Remle", "Gabby", "Olivia", "Gene"];
+
+let pendingVols = ["Rat", "Tails", "Bacon", "Brad"];
+let pendingTimes = ["2:00-5:00", "3:00 - 6:00", "6:30-7:34", "5:00-8:00"];
+
+$(".available-volunteers").hide();
+$(".pending-requests").hide();
+
+vols.forEach((item) => {
+    $(".available-volunteers").append(
+        `<div class="card border-success mb-3" style="max-width: 18rem;">
+        <div class="card-header">${item}</div>
+        <div class="card-body text-success">
+            <p class="card-text">${item}'s request description</p>
+        </div>
+</div>`);
+});
+
+pendingVols.forEach((vol)=>{
+    pendingTimes.forEach((time)=>{
+        $(".pending-requests").append(
+            `<div class="card border-info mb-3" style="max-width: 18rem;">
+            <div class="card-header">${vol}</div>
+            <div class="card-body text-info">
+              <p class="card-text">Requested Time: ${time}</p>
+            </div>
+            </div>`
+        )
+    });
+});
+
+$(".volunteers-button").click(function () {
+    $(".available-volunteers").toggle();
+});
+
+$(".requests-button").click(function () {
+    $(".pending-requests").toggle();
+});
